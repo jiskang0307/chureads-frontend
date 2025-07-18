7b12f0a3838d8055334ad841389820a3da3a5672
jiskang0307 <jiskang@krs.co.kr>
2025-07-18 15:08:51 +0900

    fix

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import PostInput from "../components/PostInput";
 import { useEffect, useState } from "react";
 import { initialFeedList } from "../data/response";
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 const Edit = () => {
   // logic
   const { id } = useParams();
